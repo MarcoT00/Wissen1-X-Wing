b1fd0f16d781cebc3e2fdeccf1319b7863dece71
MarcoT00 <tenderra.git@gmail.com>
2024-06-16 12:12:58 +0200

    Add Start and end position

--- Game/Topology.py
+++ Game/Topology.py
@@ -1,9 +1,17 @@
 import csv
 class Topology:
-    start_map_1 = []
+    start_map_1 = [[32, x] for x in range(4,10)]
+    start_map_2 = [[30, x] for x in range(1,24)]
 
-    def getStartingPos(self, map, index):
-        pass
+    end_map_1 = [[x, 17] for x in range(1,7)]
+    end_map_2 = [[x, 32] for x in range(1, 10)]
+
+    def get_starting_pos(self, selected_map, index):
+        if selected_map == 1:
+            return self.start_map_1[index]
+        elif selected_map == 2:
+            return self.start_map_2[index]
+
     @staticmethod
     def get_map(id):
         map_name = None

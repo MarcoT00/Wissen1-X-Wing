af16418cecf28cfaf300ba0d1e5241880385ac07
MarcoT00 <tenderra.git@gmail.com>
2024-06-16 12:13:20 +0200

    Rework change speed and add auswahlfunktion

--- Game/Game.py
+++ Game/Game.py
@@ -22,12 +22,13 @@ class Game:
     timestep = 0
     map = None
 
-    def __init__(self, selected_map, starting_pos):
+    def __init__(self, selected_map, starting_pos_index):
         self.map = Topology.get_map(selected_map)
+        topology = Topology()
         if selected_map == 1:
-            self.position = Topology.getStartingPos(self.map, 1, 1)
+            self.position = topology.get_starting_pos(selected_map, starting_pos_index)
         elif selected_map == 2:
-            self.position = Topology.getStartingPos(self.map, 1, 1)
+            self.position = topology.get_starting_pos(selected_map, starting_pos_index)
         self.screen = Screen(self.map)
 
     def change_position(self):
@@ -37,10 +38,21 @@ class Game:
         }
 
     def is_collide(self):
-        new_position = {
+        '''new_position = {
             "x": self.position.x + self.speed.x,
             "y": self.position.y + self.speed.y,
+        }'''
+        new_position = {
+            "x": self.position.x,
+            "y": self.position.y,
         }
+        #X-Crash
+        i = max(self.speed["x"], self.speed["y"])
+
+
+        #Y-Crash
+
+
         return not (
             0 <= new_position.x
             and len(map[0]) < new_position.x
@@ -55,16 +67,47 @@ class Game:
         pass
 
     def change_speed(self, action: tuple):
-        match action[0]:
-            case Action.B:
-                self.speed["x"] += 1
-            case Action.V:
-                self.speed["x"] -= 1
-        match action[1]:
-            case Action.B:
-                self.speed["y"] += 1
-            case Action.V:
-                self.speed["y"] -= 1
+        new_speed = self.speed
+        if self.speed["x"] in range(0, 5):
+            match action[0]:
+                case Action.B:
+                    if self.speed["x"] < 4:
+                        new_speed["x"] += 1
+                case Action.V:
+                    new_speed["x"] -= 1
+        elif self.speed["x"] in range(-4, 0):
+            match action[0]:
+                case Action.B:
+                    if self.speed["x"] > -4:
+                        new_speed["x"] -= 1
+                case Action.V:
+                    self.speed["x"] += 1
+
+        if self.speed["y"] in range(0, 5):
+            match action[1]:
+                case Action.B:
+                    if self.speed["y"] < 4:
+                        new_speed["y"] += 1
+                case Action.V:
+                    new_speed["y"] -= 1
+        elif self.speed["y"] in range(-4, 0):
+            match action[1]:
+                case Action.B:
+                    if self.speed["y"] > -4:
+                        new_speed["y"] -= 1
+                case Action.V:
+                    new_speed["y"] += 1
+        return new_speed
+
+    def get_selectable_action(self):
+        if self.position.x == 0 and self.position.y == 0:
+            return self.actions
+        else:
+            speed_prediction = {}
+            for a in self.actions:
+                speed_prediction[a] = self.change_speed(a)
+            selectable_actions = [a for a in speed_prediction.keys() if speed_prediction[a]["x"] != 0 or speed_prediction[a]["y"] != 0]
+            return selectable_actions
 
     def change_state(self, action: tuple):
         self.timestep += 1

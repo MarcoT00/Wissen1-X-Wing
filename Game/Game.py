8f265434bc3effe6018583aa2bbb54b364ed5651
Steele <minh.nguyen4@stud.fra-uas.de>
2024-06-17 11:10:47 +0200

    updated collision check and refactored

--- Game/Game.py
+++ Game/Game.py
@@ -5,7 +5,7 @@ import time
 
 
 class Game:
-    # Hyperparameters: Params that do not change during each episode
+    # Params that do not change during each episode
     ACTIONS = [
         ("B", "B"),
         ("B", "H"),
@@ -17,51 +17,67 @@ class Game:
         ("V", "H"),
         ("V", "V"),
     ]
-    NUM_EPISODES = 10
     MAP = None
-    END_POSITIONS = None
+    START_POS = None
 
-    # Parameters: Params that change during each episode
+    # Params that change during each episode
     timestep = 0
-    lern_rate = None
     velocity = {"x": 0, "y": 0}
     position = {"x": None, "y": None}
     episode_cost = 0
     episode = []  # sequence of positions and actions that the agent takes
+    screen = None
 
-    def __init__(self, map_id, starting_pos_index):
+    def __init__(self, map_id, start_pos_index):
         self.MAP = Topology.get_map(map_id)
-        self.END_POSITIONS = Topology.get_end_positions(map_id)
-        self.position = Topology.get_starting_pos(map_id, starting_pos_index)
+        self.START_POS = Topology.get_start_pos(map_id, start_pos_index)
+
+        self.position = self.START_POS
         self.episode.append(self.position)
         self.screen = Screen(self.MAP)
 
-    def reset(
+    def episode_reset(
         self,
     ):
-        # TODO: Implement this
-        pass
+        """
+        Reset the game after each episode
+        """
+        self.timestep = 0
+        self.velocity = {"x": 0, "y": 0}
+        self.position = self.START_POS
+        self.episode_cost = 0
+        self.episode = [self.position]
 
     def change_state(self, selected_action: tuple):
         self.timestep += 1
-        self.lern_rate = 1 / self.timestep
 
         # Get new velocity based on selected action
         new_velocity = self.get_new_velocity(selected_action)
 
-        # Get possible routes when moving with the new velocity
-        possible_routes = self.get_possible_routes(
-            current_position=self.position, velocity=new_velocity
+        # Get possible routes and movement sequences when moving with the new velocity
+        possible_routes, possible_movement_sequences = (
+            self.get_possible_routes_and_movement_sequences(
+                current_position=self.position, velocity=new_velocity
+            )
         )
 
-        escape_possible, escape_position = self.check_escape(possible_routes)
-        if escape_possible:
+        escape_is_possible, escape_position = self.check_escape(possible_routes)
+        if escape_is_possible:
             self.velocity = new_velocity
             self.position = escape_position
             self.episode_cost += 1
-        elif self.collision_certain(possible_routes):
-            self.velocity = self.get_velocity_after_collision(possible_routes)
-            self.position = self.get_new_position(self.velocity)
+        elif self.collision_is_certain(possible_routes):
+            while self.collision_is_certain(possible_routes):
+                velocity_after_collision = self.get_velocity_after_collision(
+                    possible_movement_sequences
+                )
+                possible_routes, possible_movement_sequences = (
+                    self.get_possible_routes_and_movement_sequences(
+                        current_position=self.position,
+                        velocity=velocity_after_collision,
+                    )
+                )
+            self.position = self.get_new_position(velocity_after_collision)
             self.episode_cost += 1 + 5
         else:
             self.velocity = new_velocity
@@ -71,9 +87,36 @@ class Game:
         self.episode.append(selected_action)
         self.episode.append(self.position)
 
-    def get_velocity_after_collision(self, possible_routes):
-        # TODO: Implement this
-        return {"x": 0, "y": 0}
+    def get_velocity_after_collision(self, possible_movement_sequences: list):
+        # Each elem of each sequence is one of the following types:
+        # (x, 1), (x, -1), (y, 1), (y, -1) : Right, Left, Up, Down
+        # All sequences have at least one type and at most two types
+        # Last elem is the movement that causes collision
+        colliding_movements = []
+        for movement_seq in possible_movement_sequences:
+            colliding_movements.append(movement_seq[-1])
+        colliding_movement_types = set(colliding_movements)
+
+        if len(colliding_movement_types) == 1:
+            match colliding_movement_types[0]:
+                case ("x", 1):
+                    return {"x": 0, "y": 1}
+                case ("x", -1):
+                    return {"x": 0, "y": 1}
+                case ("y", 1):
+                    return {"x": 1, "y": 0}
+                case ("y", -1):
+                    return {"x": 1, "y": 0}
+        else:
+            match colliding_movement_types:
+                case set([("x", 1), ("y", 1)]):
+                    return {"x": -1, "y": -1}
+                case set([("x", 1), ("y", -1)]):
+                    return {"x": -1, "y": 1}
+                case set([("x", -1), ("y", 1)]):
+                    return {"x": 1, "y": -1}
+                case set([("x", -1), ("y", -1)]):
+                    return {"x": 1, "y": 1}
 
     def check_escape(possible_routes: list):
         for route in possible_routes:
@@ -81,13 +124,13 @@ class Game:
                 return True, route[-1][0]
         return False, None
 
-    def collision_certain(possible_routes: list):
+    def collision_is_certain(possible_routes: list):
         for route in possible_routes:
             if route[-1][1] != "R":
                 return False
         return True
 
-    def get_possible_routes(self, current_position, velocity):
+    def get_possible_routes_and_movement_sequences(self, current_position, velocity):
         """
         Route: sequence of cells until no movement left or the agent has reached R or Z
         """
@@ -99,20 +142,24 @@ class Game:
         y_move = ("y", velocity["y"] / num_y_moves) if num_y_moves != 0 else ("y", 0)
         movement_list = [x_move] * num_x_moves + [y_move] * num_y_moves
         unique_permutations = set(itertools.permutations(movement_list))
-        possible_movement_sequences = [
+        available_movement_sequences = [
             list(permutation) for permutation in unique_permutations
         ]
 
         # From possible movement sequences, extract the possible routes
         # For each movement sequence, stop extracting when reaching R or Z
+        possible_movement_sequences = []
         possible_routes = []
-        for movement_seq in possible_movement_sequences:
+
+        for movement_seq in available_movement_sequences:
             route = [
                 (
                     current_position,
                     self.MAP[current_position["y"]][current_position["x"]],
                 )
             ]
+            possible_movement_seq = []
+
             for movement in movement_seq:
                 next_position = {
                     "x": route[-1][0]["x"],
@@ -121,15 +168,19 @@ class Game:
                 next_position[movement[0]] = next_position[movement[0]] + movement[1]
                 next_position_type = self.MAP[next_position["y"]][next_position["x"]]
                 route.append((next_position, next_position_type))
+                possible_movement_seq.append(movement)
                 if next_position_type in ["R", "Z"]:
                     break
+
             possible_routes.append(route)
-        return possible_routes
+            possible_movement_sequences.append(possible_movement_seq)
+
+        return possible_routes, possible_movement_sequences
 
     def get_new_position(self, velocity: dict):
         return {
             "x": self.position["x"] + velocity["x"],
-            "y": self.position["y"] + velocity["y"],
+            "y": self.position["y"] - velocity["y"],
         }
 
     def get_new_velocity(self, action: tuple):
@@ -188,6 +239,6 @@ class Game:
         self.screen.show_player()
 
 
-g = Game(map_id=1, starting_pos_index=1)
+g = Game(map_id=1, start_pos_index=1)
 g.update_screen()
 time.sleep(25)
--- Game/Topology.py
+++ Game/Topology.py
@@ -5,27 +5,20 @@ class Topology:
 
     start_positions_map_1 = [(32, x) for x in range(4, 10)]
     start_positions_map_2 = [(30, x) for x in range(1, 24)]
-    end_positions_map_1 = [(x, 17) for x in range(1, 7)]
-    end_positions_map_2 = [(x, 32) for x in range(1, 10)]
-
-    @staticmethod
-    def get_starting_pos(self, map_id, starting_pos_index):
-        if map_id == 1:
-            return self.start_positions_map_1[starting_pos_index]
-        elif map_id == 2:
-            return self.start_positions_map_2[starting_pos_index]
+    end_positions_map_1 = [(y, 17) for y in range(1, 7)]
+    end_positions_map_2 = [(y, 32) for y in range(1, 10)]
 
     @staticmethod
     def get_map(map_id):
         map_name = "topology" + str(map_id) + ".csv"
         with open(map_name) as csvfile:
             rows = csv.reader(csvfile)
-            res = [list(row) for row in rows]
-        return res
+            return [list(row) for row in rows]
 
     @staticmethod
-    def get_end_positions(self, map_id):
+    def get_start_pos(self, map_id, start_pos_index):
         if map_id == 1:
-            return self.end_positions_map_1
+            pos = self.start_positions_map_1[start_pos_index]
         elif map_id == 2:
-            return self.end_positions_map_2
+            pos = self.start_positions_map_2[start_pos_index]
+        return {"x": pos[1], "y": pos[0]}

0dec8336ea44ce46ebef78bfb72125dfcab1d689
Steele <minh.nguyen4@stud.fra-uas.de>
2024-06-25 22:11:41 +0200

    WIP

--- Agent/Agent.py
+++ Agent/Agent.py
@@ -1,4 +1,5 @@
 from ..Game.Game import Game
+from ..Game.Topology import Topology
 import json
 
 
@@ -12,13 +13,13 @@ class Agent:
     def __init__(self):
         pass
 
-    def find_optimal_policy(self, map_id, num_episode):
-        policy, init_value_function = self.init_policy_evaluation()
+    def find_optimal_policy(self, map_id, start_pos_index, num_episode):
+        policy, init_value_function, init_g = self.initialize(map_id, start_pos_index)
 
         old_policy = {}
         while policy != old_policy:
             value_function = self.evaluate_policy(
-                map_id, num_episode, policy, init_value_function
+                num_episode, policy, init_value_function, init_g
             )
             old_policy = policy
             policy = self.improve_policy(
@@ -27,13 +28,22 @@ class Agent:
 
         return policy
 
-    def init_policy_evaluation(self):
+    def initialize(self, map_id, start_pos_index):
         # state: (x, y, (x_speed, y_speed))
         # velocity range in both directions: [-4,+4]
         # MAP[y][x] must be one of S,X,Z
         # default action is always accelerate up
+        start_pos = Topology.get_start_pos(map_id, start_pos_index)
+        self.game = Game(
+            map_id=map_id,
+            x_pos=start_pos["x"],
+            y_pos=start_pos["y"],
+            x_speed=0,
+            y_speed=0,
+        )
         policy = {}
-        value_function = {}
+        init_value_function = {}
+        init_g = {}
         self.X_SIZE = len(self.game.MAP[0])
         self.Y_SIZE = len(self.game.MAP)
         for row in range(self.Y_SIZE):
@@ -48,41 +58,38 @@ class Agent:
                             )
                             action = self.game.ACTIONS[3]
                             policy[state] = action
-                            value_function[state] = 0
-        return policy, value_function
+                            init_value_function[state] = 0
+                            init_g[state] = 0
+        return policy, init_value_function, init_g
 
-    def evaluate_policy(self, map_id, num_episode, policy, init_value_function):
+    def evaluate_policy(self, num_episode, policy, init_value_function, init_g):
         t = 1
-        curr_value_function = init_value_function
-        while t <= num_episode:
-            g = {}  # TODO
-            for state in policy.keys():
-                self.game = Game(
-                    map_id=map_id,
-                    x_pos=state[0],
-                    y_pos=state[1],
-                    x_speed=state[2][0],
-                    y_speed=state[2][1],
-                )
-                g[state] = self.get_episode_cost(policy)
-            curr_value_function = self.update_value_function(
-                g, curr_value_function, learn_rate=1 / t
-            )
-            t += 1
-        return curr_value_function
+        value_function = init_value_function
 
-    def get_episode_cost(self, policy):
-        """
-        Get episode cost after applying the given policy to the game in an episode
-        """
-        # TODO
-        episode_cost = 0
+        while t <= num_episode:
+            # TODO
+            g = init_g
+            # Calculate g
+            # step 1: Get all states that were visited in the episode
+            # step 2: Calculate g for each of those state
+
+            g[state] = self.run_episode(policy)
+            value_function = self.update_value_function(
+                g, value_function, learn_rate=1 / t
+            )
+            self.game.episode_reset()
+            t += 1
+        return value_function
+
+    def run_episode(self, policy):
+        g = [0]
+        visited_states = [self.game.get_state()]
         while not self.game.is_finished():
-            state = self.game.get_state()
-            action = policy[state]
+            action = policy[self.game.get_state()]
             cost = self.game.change_state(action)
-            episode_cost += cost
-        return episode_cost
+            g.insert(0, g[0] + cost)
+            visited_states.append(self.game.get_state())
+        return g  # TODO
 
     def update_value_function(self, g, value_function, learn_rate):
         """
